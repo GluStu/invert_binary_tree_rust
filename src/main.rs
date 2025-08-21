5ece624768f2a76453245b9f3abd2bda8a85646e
GluStu <sanchitbokade8@gmail.com>
2025-08-21 20:54:15 +0530

    changes in generate and print func

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Default, Clone)]
 pub struct TreeNode<T> {
   pub val: T,
   pub left: Option<Box<TreeNode<T>>>,
@@ -22,76 +22,77 @@ impl<T> TreeNode<T> {
   }
 }
 
-#[derive(Debug)]
-enum Action<T, U> {
-    Call(T),
-    Handle(U)
-}
-
-fn generate_tree(level: usize) -> Option<Box<TreeNode<i32>>> {
-    let mut counter = 1;
-    let mut arg_stack = Vec::<Action<usize, i32>>::new();
-    let mut ret_stack: Vec<Option<Box<TreeNode<i32>>>> = Vec::new();
-
-    use Action::*;
-    arg_stack.push(Call(level));
-    while let Some(action) = arg_stack.pop() {
-        match action {
-            Call(level) => if level > 0 {
-                arg_stack.push(Handle(counter));
-                counter += 1;
-                arg_stack.push(Call(level - 1));
-                arg_stack.push(Call(level - 1));
-            } else {
-                ret_stack.push(None);
-            },
-            Handle(val) => {
-                let left = ret_stack.pop().unwrap();
-                let right = ret_stack.pop().unwrap();
-                ret_stack.push(Some(Box::new(TreeNode{val, left, right})));
-            },
-        }
+fn generate_tree(level: usize, counter: &mut i32) -> Option<Box<TreeNode<i32>>> {
+    if level == 0{
+        return None;
     }
-
-    ret_stack.pop().unwrap()
+    else{
+        let mut node = TreeNode{val: *counter, left: None, right: None};
+        node.val = *counter;
+        *counter += 1;
+        node.left = generate_tree(level - 1, counter);
+        node.right = generate_tree(level - 1, counter);
+        Some(Box::new(node.clone()))
+    }
 }
 
-fn print_tree<T: Display>(root: &Option<Box<TreeNode<T>>>) {
-    let mut stack = Vec::<Action<(&Option<Box<TreeNode<T>>>, usize), (&T, usize)>>::new();
-    use Action::*;
-    stack.push(Call((root, 0)));
-    while let Some(action) = stack.pop() {
-        match action {
-            Call((root, level)) => if let Some(node) = root {
-                stack.push(Call((&node.left, level + 1)));
-                stack.push(Handle((&node.val, level)));
-                stack.push(Call((&node.right, level + 1)));
-            },
-            Handle((value, level)) => {
-                for _ in 0..level {
-                    print!("  ")
-                }
-                println!("{}", value);
+fn print_tree<T: Display>(root: &Option<Box<TreeNode<T>>>, level: usize) {
+    match root{
+        Some(node) => {
+            print_tree(&node.left, level + 1);
+            for _ in 0..level{
+                print!("   ")
             }
-        }
+            println!("{}", &node.val);
+            print_tree(&node.right, level + 1);
+        },
+        None => {},
     }
 }
 
 fn invert_tree<T>(root: Option<Box<TreeNode<T>>>) -> Option<Box<TreeNode<T>>> {
-    match root {
-        Some(mut node) => {
-            let left = invert_tree(node.right.take());
-            let right = invert_tree(node.left.take());
-            node.left = left;
-            node.right = right;
-            Some(node)
-        }
-        None => None,
+    // match root {
+    //     Some(mut node) => {
+    //         let left = invert_tree(node.right.take());
+    //         let right = invert_tree(node.left.take());
+    //         node.left = left;
+    //         node.right = right;
+    //         Some(node)
+    //     }
+    //     None => None,
+    // }
+
+    if let Some(mut node) = root {
+        let left = invert_tree(node.right.take());
+        let right = invert_tree(node.left.take());
+        node.left = left;
+        node.right = right;
+        return Some(node);
     }
+    None
 }
 fn main() {
-    let tree = generate_tree(3);
-    print_tree(&tree);
+    let mut counter = 1;
+    let tree = generate_tree(3, &mut counter);
+    print_tree(&tree, 3);
     println!("-----------------------");
-    print_tree(&invert_tree(tree));
+    print_tree(&invert_tree(tree),3);
 }
+
+// use std::rc::Rc;
+// use std::cell::RefCell;
+// impl Solution {
+//     pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+//         if root.is_none(){
+//             return None;
+//         }
+//         if let Some(node) = root.clone(){
+//             let mut node_borr = node.borrow_mut();
+
+//             let temp = node_borr.left.clone();
+//             node_borr.left = Self::invert_tree(node_borr.right.clone());
+//             node_borr.right = Self::invert_tree(temp.clone());
+//         }
+//         root
+//     }
+// }
